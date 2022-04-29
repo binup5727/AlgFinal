be10f1e3bc17a077abfbca81ec85f9db3479d969
binup5727 <binup5727@gmail.com>
2022-04-29 18:35:25 -0400

    initial

--- Untitled-1.py
+++ Untitled-1.py
@@ -39,13 +39,14 @@ def cutBruteForce(n, p):
             maxcuts = binaryArr
         
     time = datetime.now() - timeA
-    return maxp, steps, time
+    return maxp, steps, time.microseconds
             
         
 
         
 def cutBottomUp(n, p):
-    r = [0 for i in range(n + 1)]
+    r = [0] * (n + 1)
+    
     steps = 0
     timeA = datetime.now()
     #print(r)
@@ -58,18 +59,19 @@ def cutBottomUp(n, p):
         r[j] = maxp
 
     time = datetime.now() - timeA
-    return r[n], steps, time
+    return r[n], steps, time.microseconds
 
     
+#print(cutBottomUp(4, [1,4,6,5,6,7,99]))
 
 
 
 
 
 
-
-n = [i*(10**2) for i in range(1,11)]
+n = [i*(10**1) for i in range(1,11)]
 print(n)
+n = np.array(n)
 constant = 2
 
 p = [1]
@@ -78,29 +80,99 @@ for i in range(1, n[-1]):
 
 Alg1_Steps = np.zeros(10)
 Alg2_Steps = np.zeros(10)
+
 Alg1_Time = np.zeros(10)
 Alg2_Time = np.zeros(10) 
+
+Alg1_StepRatio = np.zeros(10)
+Alg2_StepRatio = np.zeros(10)
+
+Alg1_TimeRatio = np.zeros(10)
+Alg2_TimeRatio = np.zeros(10)
+
+TheoreticalAlg1 = np.zeros(10)
+TheoreticalAlg2 = np.zeros(10)
+
 for j in range(len(n)):
     #make price values increase by constant
     
-
+    TheoreticalAlg1[j] = n[j] * (2**(n[j]-1))
+    TheoreticalAlg2[j] = n[j]**2
 
 
     profit, Alg1_Steps[j], Alg1_Time[j] = cutBruteForce(n[j], p)
+    Alg1_StepRatio[j] = Alg1_Steps[j]/TheoreticalAlg1[j]
+    Alg1_TimeRatio[j] = Alg1_Time[j]/TheoreticalAlg1[j]
 
     print(profit, Alg1_Steps[j], Alg1_Time[j])
 
 
-    profit2, Alg2_Steps[j], Alg2_Time[j] = cutBottomUp(n, p)
 
+    profit2, Alg2_Steps[j], Alg2_Time[j] = cutBottomUp(n[j], p)
+    Alg2_StepRatio[j] = Alg2_Steps[j]/TheoreticalAlg2[j]
+    Alg2_TimeRatio[j] = Alg2_Time[j]/TheoreticalAlg2[j]
+
+
+    
     print(profit2, Alg2_Steps[j], Alg2_Time[j])
 
+Alg1Cstep = max(Alg1_StepRatio)
+Alg1Ctime = max(Alg1_TimeRatio)
+
+Alg2Cstep = max(Alg2_StepRatio)
+Alg2Ctime = max(Alg2_TimeRatio)
+
+PredictedRT_Alg1_Step = Alg1Cstep * TheoreticalAlg1
+PredictedRT_Alg1_time = Alg1Ctime * TheoreticalAlg1
+
+PredictedRT_Alg2_Step = Alg2Cstep * TheoreticalAlg2
+PredictedRT_Alg2_time = Alg2Ctime * TheoreticalAlg2
+
+plt.plot(n, Alg1_Steps, label="EmpiricalRT Brute Force")
+plt.plot(n, PredictedRT_Alg1_Step, label="PredictedRT Brute Force")
+plt.xlabel("n values")
+plt.ylabel("RunTime in Steps")
+plt.legend()
+plt.show()
+
+plt.plot(n, Alg1_Time, label="EmpiricalRT Brute Force")
+plt.plot(n, PredictedRT_Alg1_time, label="PredictedRT Brute Force")
+plt.xlabel("n values")
+plt.ylabel("RunTime in Time(ms)")
+plt.legend()
+plt.show()
 
 
 
+plt.plot(n, Alg2_Steps, label="EmpiricalRT DP Bottom Up")
+plt.plot(n, PredictedRT_Alg2_Step, label="PredictedRT DP Bottom Up")
+plt.xlabel("n values")
+plt.ylabel("RunTime in Steps")
+plt.legend()
+plt.show()
+
+plt.plot(n, Alg2_Time, label="EmpiricalRT DP Bottom Up")
+plt.plot(n, PredictedRT_Alg2_time, label="PredictedRT DP Bottom Up")
+plt.xlabel("n values")
+plt.ylabel("RunTime in Time(ms)")
+plt.legend()
+plt.show()
 
 
 
+plt.plot(n, Alg1_Steps, label="EpriricalRT Brute Force")
+plt.plot(n, Alg2_Steps, label="EmpiricalRT DP Bottom-Up")
+plt.xlabel("n values")
+plt.ylabel("RunTime in Steps")
+plt.legend()
+plt.show()
+
+plt.plot(n, Alg1_Time, label="EpriricalRT Brute Force")
+plt.plot(n, Alg2_Time, label="EmpiricalRT DP Bottom-Up")
+plt.xlabel("n values")
+plt.ylabel("RunTime in Time(ms)")
+plt.legend()
+plt.show()
 
 
 
