2dba412ffb3c7c172cf972ef8787aa4f86911afb
binup5727 <binup5727@gmail.com>
2022-04-30 21:09:07 -0400

    v

--- AlgoFinalProj.py
+++ AlgoFinalProj.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+
 import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
@@ -7,14 +7,13 @@ import math
 
 
 def cutBruteForce(n, p):
-    #creates all possible cuts
-    #print(n)
+
     maxp = 0
     
     steps = 0
     num = int(math.pow(2, (n-1)))
     for i in range(num):
-        #print(2**(n-1))
+        
         binaryArr = np.array(list(np.binary_repr(i).zfill(n-1))).astype(np.int8)
         
         length2 = 0
@@ -22,14 +21,15 @@ def cutBruteForce(n, p):
         locp = 0
         for j in range(len(binaryArr)):
             steps += 1
+            print(n)
             print(steps, " of {:e}".format(num * len(binaryArr)))
             
             length2 += 1
-            #print(locp)
+           
             if binaryArr[j] == 1:
                 locp = locp + p[length2 - 1]
                 length2 = 0
-                #print(locp)
+            
             if j == n-2:
                 
                 length2 += 1
@@ -52,22 +52,23 @@ def cutBottomUp(n, p):
     
     steps = 0
     
-    #print(r)
+
     maxp = 0
     for j in range(1, n + 1):
         maxp = 0
         for i in range(j):
+            print(n)
+            print(steps, " of {:e}".format(n**2))
             steps += 1
             maxp = max(maxp, p[i] + r[j - i - 1])
         r[j] = maxp
 
     
     maxp = r[n]
-    #print(r[n])
+
     return r[n], steps
 
-    
-#print(cutBottomUp(4, [1,4,6,5,6,7,99]))
+
 
 
 
@@ -108,25 +109,15 @@ for k in range(m):
     for j in range(len(n)):
         
         tup1 = cutBruteForce(n[j], p)
-        #print(tup)
+   
         profit[j] += tup1[0]
         Alg1_Steps[j] += tup1[1]
-        
-
-        #profit[j], Alg1_Steps[j], Alg1_Time[j] += cutBruteForce(n[j], p)
+     
         tup2 = cutBottomUp(n[j], p)
         profit2[j] += tup2[0]
         Alg2_Steps[j] += tup2[1]
         
-        #print(profit, Alg1_Steps[j], Alg1_Time[j])
-
-
-
-        #profit2[j], Alg2_Steps[j], Alg2_Time[j] += cutBottomUp(n[j], p)
         
-
-        
-        #print(profit2, Alg2_Steps[j], Alg2_Time[j])
 
 
 
@@ -146,10 +137,10 @@ Alg2_StepRatio = Alg2_Steps/TheoreticalAlg2
 
 
 
-Alg1Cstep = max(Alg1_StepRatio)
+Alg1Cstep = max(Alg1_StepRatio[1:])
 
 
-Alg2Cstep = max(Alg2_StepRatio)
+Alg2Cstep = max(Alg2_StepRatio[1:])
 
 
 PredictedRT_Alg1_Step = Alg1Cstep * TheoreticalAlg1
@@ -157,15 +148,18 @@ PredictedRT_Alg1_Step = Alg1Cstep * TheoreticalAlg1
 
 PredictedRT_Alg2_Step = Alg2Cstep * TheoreticalAlg2
 
-
+n1values = pd.DataFrame({'n': TheoreticalAlg1})
+n2values = pd.DataFrame({"n": TheoreticalAlg2})
 
 Alg1data = pd.DataFrame({'n':n, 'Empirical RT steps': Alg1_Steps, 'Ratio = (Emperical RT)/(Theoretical Complexity)': Alg1_StepRatio, 'predicted RT': PredictedRT_Alg1_Step})
 
 Alg2data = pd.DataFrame({'n':n, 'Empirical RT steps': Alg2_Steps, 'Ratio = (Emperical RT)/(Theoretical Complexity)': Alg2_StepRatio, 'predicted RT': PredictedRT_Alg2_Step})
-
+print(n1values)
+print(n2values)
 print(Alg1data)
 print(Alg2data)
-
+print(Alg1Cstep)
+print(Alg2Cstep)
 
 plt.plot(n, Alg1_Steps, label="EmpiricalRT Brute Force")
 plt.plot(n, PredictedRT_Alg1_Step, label="PredictedRT Brute Force")
